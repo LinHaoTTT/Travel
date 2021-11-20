ead7915851937ddeb6bef41f81e00b6a90441838
linhao <1170513893@qq.com>
2021-11-20 15:10:27 +0800

    project inuit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--template只能暴露一个根标签，得设置一个div包裹住可使用-->
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'Home'
+  }
+</script>
+<style>
+
+</style>
