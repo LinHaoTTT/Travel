54c209b625fb15b2903a1990ee4a0485e489c710
linhao <1170513893@qq.com>
2021-11-29 20:09:13 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template lang="">
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList">
+          <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+
+  data() {
+    return{
+      recommendList:[{
+        id:'0001',
+        imgUrl:'//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      },{
+        id:'0002',
+        imgUrl:'//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      },{
+        id:'0003',
+        imgUrl:'//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      }]
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top:.2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    display:flex
+    height: 1.9rem
+
+    .item-img
+      width:1.7rem
+      height:1.7rem
+      padding:.1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height:.54rem
+        font-size:.32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color:#fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,71 @@
+<template lang="">
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl">
+          </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+
+  data() {
+    return{
+      recommendList:[{
+        id:'0001',
+        imgUrl:'//imgs.qunarzz.com/p/p14/201302/28/218ac6605f1d637f93835fbb.jpg_256x160_bfee6c2d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      },{
+        id:'0002',
+        imgUrl:'//imgs.qunarzz.com/p/p14/201302/28/218ac6605f1d637f93835fbb.jpg_256x160_bfee6c2d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      },{
+        id:'0003',
+        imgUrl:'//imgs.qunarzz.com/p/p14/201302/28/218ac6605f1d637f93835fbb.jpg_256x160_bfee6c2d.jpg',
+        title:'含往返飞机票(含税)+4晚住宿',
+        desc:'潮玩海陆空：蜈支洲岛畅玩一整天+亚龙湾热带天堂森林公园|直升机体验+帆船出海两小时激情玩海之旅（三亚连住180°海景房）'
+
+      }]
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top:.2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom:65.5%
+    .item-img
+      width:100%
+  .item-info
+    padding: .1rem
+  .item-title
+    line-height:.54rem
+    font-size:.32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #ccc
+    ellipsis()
+</style>
