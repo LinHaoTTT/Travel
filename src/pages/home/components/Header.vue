6a05cedf32e458bd4ba187afb2844f7acd39b134
linhao <1170513893@qq.com>
2021-12-01 22:15:41 +0800

    search to city

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题</div>
-    <div class="header-right">
-      城市
-      <span class="iconfont arrow-icon">&#xeb6d;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xeb6d;</span>
+      </div>
+    </router-link>
 
   </div>
 </template>
@@ -49,6 +51,7 @@
       width: 1.24rem
       float:right
       text-align:center
+      color:#fff
       .arrow-icon
         margin-left:-.04rem
         font-size: .2rem
