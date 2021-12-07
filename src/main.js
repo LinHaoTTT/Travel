e64f42e7f68167a5b7a2c875b49468058e40e088
linhao <1170513893@qq.com>
2021-12-07 16:06:30 +0800

    fix:修复问题

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App'
 import router from './router'
 import 'styles/border.css'
 import 'styles/reset.css'
-import fastClick from 'fastclick'
+
 import  VueAwesomeSwiper  from  'vue-awesome-swiper'
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 import store from './store'
 
 Vue.config.productionTip = false
-fastClick.attach(document.body)
+
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
